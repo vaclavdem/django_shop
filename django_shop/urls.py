d58a42a0f63671526b3d4a6d8c6c31eedad0ec0a
vaclavdem <85072668+vaclavdem@users.noreply.github.com>
2021-07-29 15:55:10 +0300

    Update urls.py

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -20,9 +20,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 
-    #path('auth/', include('djoser.urls')),
-    #path('auth/', include('djoser.urls.authtoken')),
-    #path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
 
     path('api/v1/', include('delivery.urls')),
 ]
