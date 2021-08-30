fa3b3c05233929536ab05d4b6ea7e857caca6286
vaclavdem <85072668+vaclavdem@users.noreply.github.com>
2021-08-30 16:06:33 +0300

    Delete delivery directory

--- delivery/service/tasks.py
+++
@@ -1,7 +0,0 @@
-from django_shop.celery import app
-from .service import send_ultimative_mail
-
-
-@app.task
-def send_email(title, text, user_email):
-    send_ultimative_mail(title, text, user_email)
